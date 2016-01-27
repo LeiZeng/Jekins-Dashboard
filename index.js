774ca9db5462303c6d1f7b5fcb89615d67e0c274
taiyu <cherrygty@gmail.com>
2016-01-27 17:21:15 +0800

    change from taiyu

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 import $ from 'jquery'
 
 import appTmp from './hbs/app.hbs'
+const duplicate = (num, obj) => {
+  const result = []
+  let i = 0
+  for (; i < num; i++) {
+    result.push(Object.assign({}, obj))
+  };
+  return result
+}
+const siteData = {
+  name: 'GIO',
+  lastBuildAuthor:'Ziyi',
+  lastBuildTime:'2016/01/28',
+  status:'pass'
+}
 const appData = {
   name: 'Taiyu',
-  people: [{
-    user: 'Ziyi'
-  },{
-    user: 'Ziyi'
-  },{
-    user: 'Ziyi'
-  },{
-    user: 'Ziyi'
-  },{
-    user: 'Ziyi'
-  },{
-    user: 'Ziyi'
-  },{
-    user: 'Ziyi'
-  },{
-    user: 'Ziyi'
-  },{
-    user: 'Ziyi'
-  }]
+  site: duplicate(9, siteData)
 }
 const app = appTmp(appData)
 
